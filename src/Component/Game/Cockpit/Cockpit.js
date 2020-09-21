228604d940f17e559ea9076dff501d8d515fde68
N-pelletant <pelletantnic@gmail.com>
2020-09-21 15:43:47 +0200

    Card display

--- src/Component/Game/Cockpit/Cockpit.js
+++ src/Component/Game/Cockpit/Cockpit.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import Card from '../Gameboard/Card/Card';
+import classes from './Cockpit.module.css';
 
-function Cockpit(props) {
+function Cockpit({name, level, currentScore, totalScore}) {
   return (
-    <div>
-      <p>{props.name}</p>
-      <p>Difficulty level : {props.level}</p>
-      <p>Current score : {props.currentScore}</p>
-      <p>Total score : {props.totalScore}</p>
+    <div className={classes.Cockpit}>
+      <div className={classes.Banner}>
+        <h2>Voltorb flip lv{level}</h2>
+      </div>
+      <Card value={0} />
+      <Card value={1} />
+      <Card value={2} />
+      <Card value={3} />
+      <Card value={0} faceUp />
+      <Card value={1} faceUp />
+      <Card value={2} faceUp />
+      <Card value={3} faceUp />
+      <p>{name}</p>
+      <p>Current score : {currentScore}</p>
+      <p>Total score : {totalScore}</p>
     </div>
   );
 }
--- src/Component/Game/Gameboard/Card/Card.js
+++ src/Component/Game/Gameboard/Card/Card.js
@@ -1,14 +1,47 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import classes from './Card.module.css';
 
-Card.propTypes = {
-  faceUp: PropTypes.bool.isRequired,
-  value: PropTypes.oneOf([1,2,3,"bomb"]).isRequired,
-  click: PropTypes.func.isRequired,
-};
+import card0 from './../../../../assets/images/cards/0.png';
+import card1 from './../../../../assets/images/cards/1.png';
+import card2 from './../../../../assets/images/cards/2.png';
+import card3 from './../../../../assets/images/cards/3.png';
 
-function Card(props) {
-  return <div></div>
+function Card({ value, faceUp }) {
+  let pic = {};
+
+  let [face, flipFace] = useState(true)
+
+  switch (value) {
+    case 0:
+      pic.src = card0;
+      pic.alt = "bomb"
+      break;
+    case 1:
+      pic.src = card1;
+      pic.alt = "1"
+      break;
+    case 2:
+      pic.src = card2;
+      pic.alt = "2"
+      break;
+    case 3:
+      pic.src = card3;
+      pic.alt = "3"
+      break;
+    default:
+      pic = null;
+  }
+
+  const addedClasses = [classes.Card]
+  if(face) addedClasses.push(classes.flip)
+
+  return (
+    <div
+      className={addedClasses.join(' ')}
+      onClick={() => flipFace(!face)}>
+      <div className={classes.Back}></div>
+      <img className={classes.Front} src={pic.src} alt={pic.alt} />
+    </div>);
 }
 
 export default Card;
